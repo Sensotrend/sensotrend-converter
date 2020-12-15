1a1af519dbffc42be0a22415eeea39f177ed8dac
Mikael Rinnetmäki <mikael@sensotrend.com>
2020-12-15 05:27:58 +0200

    Try yet another way of importing uuid

--- src/templates/fiphr/index.js
+++ src/templates/fiphr/index.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 import moment from 'moment';
 import ST from 'stjs';
 import _ from 'lodash';
-import uuidv5 from 'uuid/v5';
+import uuidv5 from 'uuid/v5.js';
 import { DataFormatConverter} from '../../DataFormatConverter.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
